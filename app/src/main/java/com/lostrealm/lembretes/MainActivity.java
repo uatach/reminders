7e0aa94dcd1b91d6f92ef89c51c26b70f4d1b933
edson duarte <edsonduarte1990@gmail.com>
2015-11-20 21:45:03 -0200

    Added dinner preferences.

--- app/src/main/java/com/lostrealm/lembretes/MainActivity.java
+++ app/src/main/java/com/lostrealm/lembretes/MainActivity.java
@@ -42,6 +42,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
     }
 
     @Override
