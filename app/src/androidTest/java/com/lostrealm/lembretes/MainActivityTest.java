7cc4ec1342ece482256aedf4c3454bb66925798f
edson duarte <edsonduarte1990@gmail.com>
2015-12-03 11:22:50 -0200

    Enabling tests on travis.

---
+++ app/src/androidTest/java/com/lostrealm/lembretes/MainActivityTest.java
@@ -0,0 +1,20 @@
+package com.lostrealm.lembretes;
+
+import android.test.SingleLaunchActivityTestCase;
+
+public class MainActivityTest extends SingleLaunchActivityTestCase<MainActivity> {
+
+    public MainActivityTest() {
+        super("com.lostrealm.lembretes", MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+}
