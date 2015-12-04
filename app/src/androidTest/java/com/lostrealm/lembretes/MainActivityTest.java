f6bf6c325404e07a1736f8fde57eb1e11248ec60
edson duarte <edsonduarte1990@gmail.com>
2015-12-04 12:41:52 -0200

    Enabling tests on travis. [4]

--- app/src/androidTest/java/com/lostrealm/lembretes/MainActivityTest.java
+++ app/src/androidTest/java/com/lostrealm/lembretes/MainActivityTest.java
@@ -31,7 +31,7 @@ public class MainActivityTest extends SingleLaunchActivityTestCase<MainActivity>
     }
 
     public void testInitialValues() {
-        final String expectedTitleText = mainActivity.getString(R.string.main_activity_label);
+        final String expectedTitleText = mainActivity.getString(R.string.main_activity_note);
         final String actualTitleText = titleTextView.getText().toString();
         assertEquals(expectedTitleText, actualTitleText);
     }
